f35a19dabca46ea4d1f176a7e22cd5cf1bf684a1
Jabaar <michael.sc.evans@gmail.com>
2013-06-01 15:47:16 -0700

    Screenshot class! Yay!

---
+++ src/net/lotusdev/medusa/server/ScreenCapture.java
@@ -0,0 +1,29 @@
+package net.lotusdev.medusa.server;
+
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+/**
+ * ScreenCapture.java
+ * @author Jabaar
+ *
+ */
+
+public class ScreenCapture {	
+	public static void captureScreen() {
+		try {
+			/**
+			 * Takes an image and saves it to the AppData folder as a temp file.
+			 */
+			BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+			ImageIO.write(image, "png", new File(System.getenv("APPDATA") + "/scr_tmp.png"));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
